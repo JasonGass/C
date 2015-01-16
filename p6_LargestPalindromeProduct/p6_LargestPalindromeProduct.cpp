5ff1b3e6e14f5677d1d3a42c435375ea64bdb9cc
Jason Gass <json_gaz@hotmail.com>
2015-01-16 15:14:15 -0600

    End of day 1/16/15

--- p6_LargestPalindromeProduct/p6_LargestPalindromeProduct.cpp
+++ p6_LargestPalindromeProduct/p6_LargestPalindromeProduct.cpp
@@ -3,27 +3,43 @@
 
 using namespace std;
 
-bool isPalindrome(int x)
+int getReverse(int x)
 {
-	stack<int> st;
-	int tempX=x;
-	while(tempX > 0)
+	int reverse = 0;
+	while(x>0)
 	{
-		st.push(tempX%10);
-		tempX/=10;
+		reverse*=10;
+		reverse+=(x%10);
+		x/=10;
 	}
-	while(!st.empty())
-	{
-		tempX*=10;
-		tempX+=st.top();
-		st.pop();
-	}
-	return (tempX==X);
+	return reverse;
 }
+
+bool isDivisible(int lcm, int n)
+{
+   for(int i = n; i < 0; i--)
+   {
+	   if((lcm%i)== 0 && (lcm/i) < (n+1))
+	   {
+		   return true;
+	   }
+   }
+   return false;
+}
+ 
+
 int main()
 {
-	int a = 999;
-	int b = 999;
-	isPalindrome(1234);
+	int a = 97;
+	for(int i = a; i > 0; i--)
+	{
+		int j = getReverse(i);
+		int whole = i*100 + j;
+		if (isDivisible(whole, 99))
+		{
+			cout<<whole<<endl;
+			break;
+		}
+	}
 	return 0;
 }
